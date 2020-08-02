047b6e9cfd3dddc17324f2d50d811b87fbe5bd8b
Вадим <vadMoroz13@yandex.ru>
2020-08-02 13:45:23 +0300

    Старт

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+let money, income, addExpenses, deposit, mission, period, budgetDay;
+
+money = 50000;
+income = 'фриланс';
+addExpenses = 'Интернет, Такси, Комунальные платежи';
+deposit = true;
+mission = 5000000;
+period = 12;
+budgetDay = money/30
+
+console.log( typeof money);
+console.log( typeof income);
+console.log( typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split());
+console.log(budgetDay);
